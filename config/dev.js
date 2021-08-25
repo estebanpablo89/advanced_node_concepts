c8813c87fcc1abb7ca1f99d2d8c27799bab4a85b
estebanpablo89 <estebanpablo89@gmail.com>
2021-08-25 12:08:27 -0500

    db

--- config/dev.js
+++ config/dev.js
@@ -3,6 +3,6 @@ module.exports = {
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
   mongoURI:
-    'mongodb+srv://web:mongoweb@estebancluster.wtfwr.mongodb.net/advanced_node?retryWrites=true&w=majority',
+    'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
   cookieKey: '123123123',
 };
