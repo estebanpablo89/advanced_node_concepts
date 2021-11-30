da59bb7a6e32fcbcdfd6c28d75bf7c641835bc06
Esteban <estebanpablo89@gmail.com>
2021-11-30 15:28:32 -0500

    Restoring Blog Routes Handler

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,28 +14,9 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const redis = require('redis');
-    const redisUrl = 'redis://localhost:6379';
-    const client = redis.createClient(redisUrl);
-    const util = require('util');
-    client.get = util.promisify(client.get);
-
-    // do we have any chached data in redis related to this query
-    const cachedBlogs = await client.get(req.user.id);
-
-    // if yes, then respond to the request right away and return
-    if (cachedBlogs) {
-      console.log('SERVING FROM CACHE');
-      return res.send(JSON.parse(cachedBlogs));
-    }
-
-    // if no, we need to respond to request and update our cache to store the data
-
-    const blogs = await Blog.find({ _user: req.user.id });
-    console.log('SERVING FROM MONGODB');
-    res.send(blogs);
-
-    client.set(req.user.id, JSON.stringify(blogs));
+    const blogs = await Blog.find({ _user: req.user.id })
+    
+    res.send(blogs)
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
