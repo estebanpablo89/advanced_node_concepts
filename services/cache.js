bff4bf82ce06a2a1e28cbceab6d441a9bf999c87
Esteban <estebanpablo89@gmail.com>
2021-12-07 22:54:50 -0500

    Unique keys

--- services/cache.js
+++ services/cache.js
@@ -1,8 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const exec = mongoose.Query.prototype.exec;
+const exec = mongoose.Query.prototype.exec
 
 mongoose.Query.prototype.exec = function () {
-    console.log('About to run a query');
-    return exec.apply(this, arguments);
+    console.log('About to run a query')
+
+    console.log(this.getQuery())
+    console.log(this.mongooseCollection.name)
+
+    return exec.apply(this, arguments)
 }
