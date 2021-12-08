bb818ec21efb3b8061d0916cb89f67b406354f91
Esteban <estebanpablo89@gmail.com>
2021-12-08 15:23:26 -0500

    Cache Implementation

--- services/cache.js
+++ services/cache.js
@@ -1,12 +1,27 @@
 const mongoose = require('mongoose')
+const redis = require('redis')
+const util = require('util')
 
+const redisUrl = 'redis://127.0.0.1:6379'
+const client = redis.createClient(redisUrl)
+client.get = util.promisify(client.get)
 const exec = mongoose.Query.prototype.exec
 
-mongoose.Query.prototype.exec = function () {
-    console.log('About to run a query')
+mongoose.Query.prototype.exec = async function () {
+    const key = JSON.stringify({
+        ...this.getQuery(), 
+        collection: this.mongooseCollection.name
+    })
 
-    console.log(this.getQuery())
-    console.log(this.mongooseCollection.name)
+    // See if we have a vlue for 'key' in redis
+    const cacheValue = await client.get(key)
 
-    return exec.apply(this, arguments)
+    // if we do, return that
+    if (cacheValue) {
+        console.log(cacheValue)
+    }
+
+    // otherwise, issue the query and store the result in redis
+    const result = await exec.apply(this, arguments)
+    console.log(result)
 }
